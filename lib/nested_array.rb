60826107f07983018ce078f7178b1ffa8ad55a19
Duke Ko <dukhyunko93@gmail.com>
2020-06-09 17:47:38 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,6 +35,6 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  new_value = matrix[row][column]= "Lemons"
+  new_value = matrix[row][column] = "Lemons"
   new_value
 end
